0af1d0f85aac9536647c442e9c11ed3477a14c1c
tejas-salespoint <tejas.lade@salespintinc.com>
2023-11-08 22:29:47 +0530

    icon color

--- src/components/ToggleButton.jsx
+++ src/components/ToggleButton.jsx
@@ -12,7 +12,7 @@ const ToggleButton = ({ value, setValue }) => {
         className="sr-only peer"
       />
       <div className="w-11 h-6   rounded-full peer bg-neutral-400 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-      <span className="ml-3 text-sm font-bold font-magistral tracking-widest  text-gray-900 dark:text-gray-300">
+      <span className="ml-3 text-sm font-bold font-magistral tracking-widest  text-white ">
         Try your own data
       </span>
     </label>
